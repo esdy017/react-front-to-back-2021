7c1828f926a092d70889f332ed39fb2d03d50547
esdy017 <suvodipghosh17@gmail.com>
2021-12-30 14:07:06 +0530

    github Context

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,28 +1,16 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
+import GithubContext from "../../context/github/GithubContext";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+const { users, loading, fetchUsers } = useContext(GithubContext);  
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    // const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-    //   headers: {
-    //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    //   },
-    // });
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false);
-    //  console.log(data)
-  };
+  
 
   if (!loading) {
     return (
--- github-finder/src/index.js
+++ github-finder/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { GithubProvider } from './context/github/GithubContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App/>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <App />
+      </Router>
+    </GithubProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
