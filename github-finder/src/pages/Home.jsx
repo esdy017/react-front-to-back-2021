d774324e320c3fb3fc04581770930750bac82963
esdy017 <suvodipghosh17@gmail.com>
2021-12-30 12:32:46 +0530

    showing userItems

--- github-finder/src/pages/Home.jsx
+++ github-finder/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import UserResults from "../components/users/UserResults";
+
 function Home() {
     return (
-        <>
-            <h1 className="text-6xl">Welcome</h1>
-        </>
-    )
+      <>
+      {/* search component */}
+        <UserResults/>
+      </>
+    );
 }
 
 export default Home
